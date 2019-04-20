4f45260c245c20839b77b98fa84311b81eb650f7
Keith Cheveralls <keith.chev@gmail.com>
2019-04-20 10:26:19 -0700

    comments/doc and eliminate Activity.load

--- cypy2/utils/weighted_linregress.py
+++ cypy2/utils/weighted_linregress.py
@@ -3,8 +3,20 @@ import numpy as np
 
 def weighted_linregress(x, y, w):
     '''
-    TODO: write doc
+    Direct (numpy) implementation of one-dimensional weighted linear regression 
+
+    Parameters
+    ----------
+    x : 1xN array of x values
+    y : 1xN array of y values
+    w : 1xN array of weights; assumed to sum to one
+    
+    Returns
+    -------
+    The slope, offset, and RMSE (mean of the squared residuals)
+
     '''
+
     X = np.concatenate((np.ones((len(x), 1)), x[:, None]), axis=1)
     Xt = X.transpose()
     Y = y[:, None]
