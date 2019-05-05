a83641fbf6e1fd4fd3659a492e7c9ae45c6927af
Keith Cheveralls <keith.chev@gmail.com>
2019-05-04 17:21:05 -0700

    dev work on OSM and elevation: finding OSM roads traversed by a trajectory, merging OSM roads and finding intersections, better elevation interp, and how to merge NED13 tiles

--- cypy2/database/cypy2_queries.sql
+++ cypy2/database/cypy2_queries.sql
@@ -37,3 +37,56 @@ SELECT 'id' as id, ST_Collect(ST_Simplify(geom, .0001)) FROM roads
 	ST_Intersects(
         ST_MakeEnvelope(-123.31, 38.4, -122.94, 38.62, 4326), geom)
 group by id
+
+-- extract points and order from a linestring
+-- (note that ST_DumpPoints returns a set)
+select name, path, ST_AsText(geom) from (
+	select name, (ST_DumpPoints(ST_Simplify(geom, .001))).* 
+	FROM roads WHERE name like 'King Ridge %') temp
+
+
+-- find all intersections between OSM roads
+with cropped_roads as (
+	select name, geom from roads
+	WHERE name is not null and code < 5130 and
+	ST_Contains(ST_MakeEnvelope(-123.1, 38.4, -122.9, 38.5, 4326), geom)
+)
+select ST_Collect(geom) from (
+	select ST_Intersection(a.geom, b.geom) geom
+	from cropped_roads a, cropped_roads b
+	where a.name != b.name and ST_Intersects(a.geom, b.geom)) as ints
+
+
+-- count segments of roads in berkeley
+select name, count(name) from roads
+where ST_Intersects(ST_MakeEnvelope(-122.3, 37.86, -122.15, 37.923, 4326), geom)
+group by name
+order by count desc
+
+-- straighforward way to merge all segments of the same road in an ROI
+select name, ST_LineMerge(ST_Union(geom)) from (
+	select a.name as name, a.geom as geom 
+	from roads a, roads b
+	where a.name = b.name
+	and a.osm_id != b.osm_id
+	and ST_Intersects(a.geom, b.geom)
+	and ST_Intersects(ST_MakeEnvelope(-122.3, 37.86, -122.15, 37.923, 4326), a.geom)
+) as ints
+group by name
+order by name
+
+-- a faster way to merge all segments of the same road,
+-- by doing the cross join only on the roads in the ROI
+with tmp as (
+	select * from roads
+	where ST_Intersects(ST_MakeEnvelope(-122.3, 37.86, -122.15, 37.923, 4326), geom)
+)
+select name, ST_LineMerge(ST_Union(geom)) from (
+	select a.name as name, a.geom as geom 
+	from tmp a, tmp b
+	where a.name = b.name
+	and a.osm_id != b.osm_id
+	and ST_Intersects(a.geom, b.geom)
+) as ints
+group by name
+order by name
